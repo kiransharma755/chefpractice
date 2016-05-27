3536fea84d7a7bb6ecf215e621b34f83c2d51887
root <root@datanode2.bipratech.com>
2016-05-27 19:03:55 +0000

    all chef dir

---
+++ git.sh
@@ -0,0 +1,6 @@
+echo "# chef" >> README.md
+i#git init
+git add . 
+git commit -m "chef learning"
+git remote add origin git@github.com:kiransharma755/chef.git
+git push -u origin master
---
+++ solo.rb
@@ -0,0 +1,2 @@
+cookbook_path '/var/chef/cookbooks'
+data_bag_path '/var/chef/data_bags'
